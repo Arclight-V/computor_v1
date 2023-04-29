05b91ee11ba6e44a3617dd44be6a5ee97ed94c6e
VVKalanchin <vladimtor@gmail.com>
2023-04-29 18:30:51 +0300

    refactoring

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <iomanip>
+
+#include "src/Computor/Computor_v1.h"
+
+int main(int argc, char* argv[]) {
+
+    if (argc != 2) {
+        std::cerr << "# " << std::setw(std::strlen("Example:")) << "Error: " << "missing equation\n";
+        std::cerr << std::setw(std::strlen("Example:") + 2)
+                  << "Example: " << "./computor_v1 \"5 * X^0 + 4 * X^1 - 9.3 * X^2 = 1 * X^0\"\n";
+        exit(EXIT_FAILURE);
+    }
+    std::string str(argv[1]);
+    Computor_v1* computorV1 = new Computor_v1;
+    computorV1->LexicalAnalyzer(str);
+
+
+    return EXIT_SUCCESS;
+}
